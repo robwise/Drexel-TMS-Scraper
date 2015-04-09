b596b2015b54778bb715088ca3e26ee3dcdd4346
Rob Wise <robert.wise@outlook.com>
2015-04-09 00:12:10 -0400

    Fix bug causing terms not to work

--- terms.js
+++ terms.js
@@ -10,7 +10,7 @@ module.exports.list = function(req, res) {
 
         select_options.each(function(index, select_option) {
             var value = select_option.attribs.value; // e.g., "7"
-            var pretty_name = select_option.val(); // e.g., "Spring Quarter 14-15"
+            var pretty_name = select_option.children[0].data; // e.g., "Spring Quarter 14-15"
             terms[value] = pretty_name;
         });
         delete terms['0']; // remove 'Select a Term' default option
