0698b069bb5bee6b39b5bf12ef2c06db05799b5a
Rob Wise <robert.wise@outlook.com>
2015-04-10 01:45:55 -0400

    Add handling of not finding given course

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var courses = require('./courses');
 var terms = require('./terms');
 var course = require('./course');
+var infoCourses = require('./info_courses');
 
 var app = express();
 
@@ -9,6 +10,7 @@ var app = express();
 app.get('/terms', terms.list);
 app.get('/courses', courses.getCourses);
 app.get('/course/:number', course.getCourse);
+app.get('/infocourses', infoCourses.getAllInfoCourses);
 
 // Start app
 var port = process.env.PORT || 5000;
--- course.js
+++ course.js
@@ -7,13 +7,14 @@ var courses = require('./courses');
 var util = require('./util');
 
 module.exports.getCourse = getCourse;
+module.exports.buildDetailedInfoCourse = buildDetailedInfoCourse;
 
 function getCourse(req, res) {
     // Get all courses with the given course number
     var number = req.params.number;
     buildDetailedInfoCourse(number, function(infoCourse) {
         if (null === infoCourse) {
-            res.status(404).json('Could not find INFO ' + number + '.');
+            res.status(404).json('Could not find course INFO ' + number + '.');
         } else {
             res.json(infoCourse);
         }
@@ -21,7 +22,6 @@ function getCourse(req, res) {
 }
 
 function buildDetailedInfoCourse(courseNumber, callbackFn) {
-    // Get all courses with the given course number
     var term = 7;
     courses.buildCoursesFromSearch(term, '', courseNumber, '', function(courses){
         retrieveInfoCourseFromCourses(courses, function(matchingCourse) {
@@ -49,6 +49,10 @@ function retrieveInfoCourseFromCourses(courses, callbackFn) {
 // Passes version of the given course that is decorated with additional details
 // as an argument to the callback
 function addDetailsToCourse(course, callbackFn) {
+    if (null === course || null === course.detailsURL) {
+        callbackFn(null);
+        return;
+    }
     request(course.detailsURL, function(error, request, body) {
         $ = cheerio.load(body);
 
